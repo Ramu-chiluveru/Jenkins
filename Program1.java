ccf80d8656bb2184ac7a9678e12f86ccb3fa3f04
Ramu chiluveru <101164811+Ramu-chiluveru@users.noreply.github.com>
2025-04-09 15:41:30 +0530

    Update Program1.java

--- Program1.java
+++ Program1.java
@@ -4,6 +4,6 @@ import java.util.*;
 public class Program1{
 	public static void main(String args[])
 	{
-		System.out.println("Hello Jenkins");
+		System.out.println("Hello Jenkins  from master");
 	}
 }
